da0903c0ba88355d6c784f37e85d3ef20da3c1d9
Vlad F <vlad.faget@outlook.com>
2023-11-08 20:13:25 +0100

    services maybe

--- DataLayer/Services/PeopleService.cs
+++ DataLayer/Services/PeopleService.cs
@@ -8,34 +8,47 @@ namespace DataLayer.Services;
 public class PeopleService : IPeopleService
 {
     private IPeopleRepository _peopleRepository;
+    private ObjectMapper _mapper;
+    
 
     public PeopleService(IPeopleRepository peopleRepository)
     {
         _peopleRepository = peopleRepository;
+        _mapper = new ObjectMapper();
     }
 
     public async Task<List<GetAllPersonDTO>> GetAllPerson()
     {
-        throw new NotImplementedException();
+        var getAll = await _repository.GetAll();
+        return _mapper.GetAllPersonDTO(getAll) ?? new List<GetAllPersonDTO>();
     }
 
     public async Task<GetOnePersonDTO> GetOnePerson(string id)
     {
-        throw new NotImplementedException();
+        var getOne = await _repository.GetById(id);
+        return _mapper.GetOnePersonDTO(getOne);
     }
 
     public async Task<UpdatePersonDTO> UpdatePerson(string id, AlterPersonDTO person)
     {
-        throw new NotImplementedException();
+        _ = await _repository.GetById(id);
+        var old  = _mapper.AlterPersonDTO(person);
+        old.Id = id;
+        await _repository.Update(ep);
+        var updated = await _repository.GetById(old.Id);
+        return _mapper.UpdatePersonDTO(updated);
     }
 
-    public async Task<Person> AddPerson(AlterPersonDTO person)
+    public async Task<GetOnePersonDTO> AddPerson(AlterPersonDTO person)
     {
-        throw new NotImplementedException();
+        var added = await _repository.Add(_mapper.AlterPersonDTO(person));
+        return _mapper.GetOnePersonDTO(added);
     }
 
     public async Task<bool> DeletePerson(string id)
     {
-        throw new NotImplementedException();
+        var entity = await _repository.GetById(id) ?? throw new KeyNotFoundException($"No entity found with id {id}");
+
+        return await _repository.Delete(entity);
     }
 }
