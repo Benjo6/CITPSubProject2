386ddef4f74db4f1754d7615a0858a885eaf113e
vladaandrei228 <vladandreigruia2000@gmail.com>
2023-11-08 19:11:44 +0200

    person implementation

--- Common/Mapper/ObjectMapper.cs
+++ Common/Mapper/ObjectMapper.cs
@@ -15,11 +15,15 @@ public partial class ObjectMapper
     public partial EpisodeDTO EpisodeToEpisodeDTO(Episode episode);
     
     public partial AlterResponseMovieDTO MovieToAlterResponseMovieDTO(Movie movie);
-    public partial Movie AlterMovieDTOToMovie(AlterMovieDTO movie);
 
+    public partial UpdatePersonDTO PersonToUpdatePersonDTO(Person person);
     public partial Movie AlterMovieDTOToMovie(AlterMovieDTO movie);
+    public partial Person AlterPersonDTOToPerson(AlterPersonDTO person);
     public partial List<GetAllMovieDTO> MovieToGetAllMoviesDTO(List<Movie> movie);
+
+    public partial List<GetAllPersonDTO> PersonToGetAllPersonsDTO(List<Person> person);
     public partial GetOneMovieDTO MovieToGetOneMovieDTO(Movie movie);
+    public partial GetOnePersonDTO PersonToGetOnePersonDTO(Person person);
     public partial RatingHistoryDTO RatingHistoryToRatingHistoryDTO(RatingHistory ratingHistory);
     public partial RoleDTO RoleToRoleDTO(Role role);
     public partial SearchHistoryDTO SearchHistoryToSearchHistoryDTO(SearchHistory searchHistory);
--- DataLayer/Repositories/PeopleRepository.cs
+++ DataLayer/Repositories/PeopleRepository.cs
@@ -3,37 +3,163 @@ using Common.Domain;
 using DataLayer.Generics;
 using DataLayer.Infrastructure;
 using DataLayer.Repositories.Contracts;
+using Microsoft.EntityFrameworkCore;
+using Npgsql;
+using System.Xml.Linq;
 
 namespace DataLayer.Repositories;
 
 public class PeopleRepository : GenericRepository<Person>, IPeopleRepository
 {
+    private readonly AppDbContext _context;
     public PeopleRepository(AppDbContext context) : base(context)
     {
+        _context = context;
     }
 
     public async Task<List<ActorBy>> FindActorsByName(string name)
     {
-        throw new NotImplementedException();
+        using (var command = _context.Database.GetDbConnection().CreateCommand()) 
+        {
+            var query = "select * from find_actors_by_name(@name)";
+            command.CommandText = query;
+            command.Parameters.Add(new NpgsqlParameter("name", name));
+            _context.Database.OpenConnection();
+
+            using (var result = await command.ExecuteReaderAsync()) 
+            {
+                var actors = new List<ActorBy>();
+
+                while (await result.ReadAsync()) 
+                {
+                    var actorBy = new ActorBy
+                    {
+                        Id = result.GetString(result.GetOrdinal("id")),
+                        Name = result.GetString(result.GetOrdinal("name"))
+                    };
+
+                    actors.Add(actorBy);
+                }
+
+                return actors;
+            }
+        }
     }
 
     public async Task<List<ActorBy>> FindActorsByMovie(string movieId)
     {
-        throw new NotImplementedException();
+        using (var command = _context.Database.GetDbConnection().CreateCommand())
+        {
+            var query = "select * from find_actors_by_movie(@movieId)";
+            command.CommandText = query;
+            command.Parameters.Add(new NpgsqlParameter("movieId", movieId));
+            _context.Database.OpenConnection();
+
+            using (var result = await command.ExecuteReaderAsync())
+            {
+                var actors = new List<ActorBy>();
+
+                while (await result.ReadAsync())
+                {
+                    var actorBy = new ActorBy
+                    {
+                        Id = result.GetString(result.GetOrdinal("id")),
+                        Name = result.GetString(result.GetOrdinal("name"))
+                    };
+
+                    actors.Add(actorBy);
+                }
+
+                return actors;
+            }
+        }
     }
 
     public async Task<List<PopularActor>> GetPopularActorsInMovie(string movieId)
     {
-        throw new NotImplementedException();
+        using (var command = _context.Database.GetDbConnection().CreateCommand())
+        {
+            var query = "select * from getpopularactorsinmovie(@movieId)";
+            command.CommandText = query;
+            command.Parameters.Add(new NpgsqlParameter("movieId", movieId));
+            _context.Database.OpenConnection();
+
+            using (var result = await command.ExecuteReaderAsync())
+            {
+                var actors = new List<PopularActor>();
+
+                while (await result.ReadAsync())
+                {
+                    var popularActor = new PopularActor
+                    {
+                        ActorName = result.GetString(result.GetOrdinal("actor_name")),
+                        AverageRating = result.GetDecimal(result.GetOrdinal("average_rating"))
+                    };
+
+                    actors.Add(popularActor);
+                }
+
+                return actors;
+            }
+        }
     }
 
     public async Task<List<PopularCoPlayer>> GetPopularCoPlayers(string actorName)
     {
-        throw new NotImplementedException();
+        using (var command = _context.Database.GetDbConnection().CreateCommand())
+        {
+            var query = "select * from getpopularcoplayers(@actorName)";
+            command.CommandText = query;
+            command.Parameters.Add(new NpgsqlParameter("actorName", actorName));
+            _context.Database.OpenConnection();
+
+            using (var result = await command.ExecuteReaderAsync())
+            {
+                var actors = new List<PopularCoPlayer>();
+
+                while (await result.ReadAsync())
+                {
+                    var coPopularActor = new PopularCoPlayer
+                    {
+                        CoActorName = result.GetString(result.GetOrdinal("co_actor_name")),
+                        AverageRating = result.GetDecimal(result.GetOrdinal("average_rating"))
+                    };
+
+                    actors.Add(coPopularActor);
+                }
+
+                return actors;
+            }
+        }
     }
 
     public async Task<List<PersonWord>> PersonWords(string word, int frequency)
     {
-        throw new NotImplementedException();
+        using (var command = _context.Database.GetDbConnection().CreateCommand())
+        {
+            var query = "select * from person_words(@word, @frequency)";
+            command.CommandText = query;
+            command.Parameters.Add(new NpgsqlParameter("word", word));
+            command.Parameters.Add(new NpgsqlParameter("frequency", frequency));
+            _context.Database.OpenConnection();
+
+            using (var result = await command.ExecuteReaderAsync())
+            {
+                var actors = new List<PersonWord>();
+
+                while (await result.ReadAsync())
+                {
+                    var personWord = new PersonWord
+                    {
+                        Word = result.GetString(result.GetOrdinal("word")),
+                        Frequency = result.GetInt32(result.GetOrdinal("frequency"))
+                    };
+
+                    actors.Add(personWord);
+                }
+
+                return actors;
+            }
+        }
     }
 }
