a4e29f9cbcf1b7f711b3e5e0884138cc5212c6f0
vladaandrei228 <vladandreigruia2000@gmail.com>
2023-12-14 00:34:18 +0200

    asd

--- frontend/src/components/Picture/card.jsx
+++ frontend/src/components/Picture/card.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import {Link} from 'react-router-dom';
+import { AiFillStar, AiOutlineStar} from 'react-icons/ai';
 
 function BasicExample(props) {
   const placeholderImage = 'https://via.placeholder.com/400';
@@ -11,14 +12,14 @@ function BasicExample(props) {
   return (
     <div className='m-auto'>
       
-      <Card style={{ width: '10rem', height: '25rem', background: '#050505'}} key={props.Id} className='m-3'><Link to={`/movie/${props.id}`} className='h-full w-full shadow absolute z-10'>
-      <Card.Img variant="top" src={props.Poster !== "N/A" ? props.Poster : placeholderImage} alt={props.title} style={{ width: '10rem' , height: '12rem'}} />
+      <Card style={{ width: '10rem', height: '25rem', background: '#050505'}} key={props.Id} className='m-3'><Link to={`/movie/${props.id}`} className='h-full w-full shadow absolute z-10'><button className="absolute bg-black text-white right-0 rounded-full text-xl"><AiOutlineStar/></button>
+      <Card.Img variant="top" src={props.Poster !== "N/A" ? props.Poster : placeholderImage} alt={props.title} style={{ width: '10rem' , height: '12rem'}} className='img object-cover' />
       <Card.Body>
         <Card.Title><span>{truncate(props.title)}</span></Card.Title>
         <Card.Text><span>Runtime: {props.runtime ? `${props.runtime} min` : 'N/A'}</span></Card.Text>
         <Card.Text><span>Adult: {props.isAdult ? 'Yes' : 'No'}</span></Card.Text>
         <Card.Text><span>Rating: {props.rating ? props.rating : 'N/A'}</span></Card.Text>
-        <Button><i class="bi bi-star"></i></Button>
+        
       </Card.Body></Link>
     </Card>
     </div>
